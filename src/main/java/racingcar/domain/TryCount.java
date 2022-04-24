96203dd9e6290dc99ee34e8811622141324a6f2e
gunan <power4889@gmail.com>
2022-04-24 23:08:56 +0900

    refactor: 이동 횟수는 숫자만 입력 가능하다

--- src/main/java/racingcar/domain/TryCount.java
+++ src/main/java/racingcar/domain/TryCount.java
@@ -3,10 +3,20 @@ package racingcar.domain;
 public class TryCount {
 	int count;
 
-	public TryCount(int count) {
+	public TryCount(String input) {
+		int count = parseInt(input);
 		if (count <= 0) {
 			throw new IllegalArgumentException("이동 횟수는 0보다 큰 숫자여야 합니다.");
 		}
 		this.count = count;
+	}
+
+	private int parseInt(String input) {
+		try {
+			return Integer.parseInt(input);
+		}
+		catch (NumberFormatException e) {
+			throw new IllegalArgumentException("이동 횟수는 숫자만 입력 가능합니다.");
+		}
 	}
 }
--- src/test/java/racingcar/domain/TryCountTest.java
+++ src/test/java/racingcar/domain/TryCountTest.java
@@ -10,14 +10,21 @@ public class TryCountTest {
 	@DisplayName("이동 횟수를 생성한다")
 	@Test
 	void createTest() {
-		assertThat(new TryCount(1)).isNotNull();
+		assertThat(new TryCount("1")).isNotNull();
+	}
+
+	@DisplayName("이동 횟수는 숫자만 입력 가능")
+	@Test
+	void notNumberTest() {
+		assertThatThrownBy(() -> new TryCount("a"))
+				.isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@DisplayName("이동 횟수는 0보다 큰 숫자여야 한다")
 	@Test
 	void over0Test() {
-		assertThatThrownBy(() -> new TryCount(0))
+		assertThatThrownBy(() -> new TryCount("0"))
 				.isInstanceOf(IllegalArgumentException.class);
-		assertThat(new TryCount(1)).isNotNull();
+		assertThat(new TryCount("1")).isNotNull();
 	}
 }
